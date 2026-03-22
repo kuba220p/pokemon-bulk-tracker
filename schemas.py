41926852e20b34931a3dae8dbde7bdd70eafe810
Jakub Szymczyk <kubaszymczyk231@gmail.com>
2026-03-22 17:58:25 +0100

    updated schema

--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
 class PokemonCard:
-    def __init__(self, name: str, card_no: int, count: int, parent_set: str):
+    def __init__(self, name: str, card_no: int, count: int, parent_set: str, is_reverse: bool):
         self._name = name
         self._card_no = card_no
         self._count = count
@@ -18,7 +18,7 @@ class PokemonCard:
         return self._parent_set
     
     def __str__(self) -> str:
-        return f"Pokemon: {self.name()}, Set: {self.parent_set()}, Card No.: {self.card_no()}"
+        return f'Pokemon: {self.name()}, Set: {self.parent_set()}, Card No.: {self.card_no()}'
     
 class ReverseHolo(PokemonCard):
     def __init__(self, reverse_type: str, *args, **kwargs) -> None:
@@ -29,4 +29,4 @@ class ReverseHolo(PokemonCard):
         return self._reverse_type
     
     def __str__(self) -> str:
-        return super().__str__() + f" Reverse Type: {self.reverse_type()}"
+        return super().__str__() + f' Reverse Type: {self.reverse_type()}'
