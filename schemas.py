fb99e2eb5a098e355e5f35c3a505a461aa9bcdae
Jakub Szymczyk <kubaszymczyk231@gmail.com>
2026-03-22 16:50:37 +0100

    initial schema

---
+++ schemas.py
@@ -0,0 +1,32 @@
+class PokemonCard:
+    def __init__(self, name: str, card_no: int, count: int, parent_set: str):
+        self._name = name
+        self._card_no = card_no
+        self._count = count
+        self._parent_set = parent_set
+ 
+    def name(self) -> str:
+        return self._name
+    
+    def card_no(self) -> int:
+        return self._card_no
+    
+    def count(self) -> int:
+        return self._count
+
+    def parent_set(self) -> str:
+        return self._parent_set
+    
+    def __str__(self) -> str:
+        return f"Pokemon: {self.name()}, Set: {self.parent_set()}, Card No.: {self.card_no()}"
+    
+class ReverseHolo(PokemonCard):
+    def __init__(self, name: str, card_no: int, count: int, parent_set: str, reverse_type: str) -> None:
+        super().__init__(name, card_no, count, parent_set)
+        self._reverse_type = reverse_type
+        
+    def reverse_type(self) -> str:
+        return self._reverse_type
+    
+    def __str__(self) -> str:
+        return super().__str__() + f"Reverse Type: {self.reverse_type()}"
